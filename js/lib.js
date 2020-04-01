c04124d3f657c5d162ca5571950813b46320cde9
yotayokuto <yotayokuto@gmail.com>
2020-04-02 00:51:04 +0900

    fix: 句点に対応 + 複数の句読点があった際にずれないように修正

--- js/lib.js
+++ js/lib.js
@@ -11,6 +11,10 @@ $(function(){
 
     var row_count = 20;
     var col_count = 20;
+    var hanging_list = {
+        '、': 'hanging_comma',
+        '。': 'hanging_period',
+    }
 
     // テスト
     $mpi.text('　昔々、とある国のある城に王さまが住んでいました。王さまはぴっかぴかの新しい服が大好きで、服を買うことばかりにお金を使っていました。王さまののぞむことといったら、いつもきれいな服を着て、みんなにいいなぁと言われることでした。戦いなんてきらいだし、おしばいだって面白くありません。だって、服を着られればそれでいいんですから。新しい服だったらなおさらです。一時間ごとに服を着がえて、みんなに見せびらかすのでした。ふつう、めしつかいに王さまはどこにいるのですか、と聞くと、「王さまは会議室にいらっしゃいます。」と言うものですが、ここの王さまはちがいます。「王さまは衣装いしょう部屋にいらっしゃいます。」と言うのです。');
@@ -27,23 +31,26 @@ $(function(){
     });
 
     function updateText() {
-        var html = $mpi.html().replace(/^([^<]+)/g, "<div>$1</div>");
+        var html = $mpi.html().replace(/^([^<]+)/g, "<div>$1</div>"); // 最初の列がdivで囲われないので囲う。
         var $html = $(html);
         var $res = $('<div>');
 
         $html.filter('div').each(function(index) {
             var str = $(this).html();
-            var p = /[、]/g;
-            var result, indexs = [];
+            var p = /[、。]/g;
+            var result, target, indexs = [];
 
+            var count = 0;
             while (result = p.exec(str)) {
-                indexs.push(result.index);
+                if ((result.index - count) % col_count === 0) {
+                    indexs.push(result.index);
+                    count++;
+                }
             }
 
             for (var i = indexs.length - 1; i >= 0; i--) {
-                if (indexs[i] % col_count === 0) {
-                    str = strReplace(str, indexs[i], '<span>、</span>');
-                }
+                target = str[indexs[i]];
+                str = strReplace(str, indexs[i], '<span class="' + hanging_list[target] + '">' + target + '</span>');
             }
             $res.append($('<div>' + str + '</div>'));
         });
